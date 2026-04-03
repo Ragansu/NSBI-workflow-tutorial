updates for kl (runs but graph are stats wrong)

src/nsbi_common_utils/models/sbi_parametric_model.py
@@ -83,6 +83,7 @@ class sbi_parametric_model:
         self.initial_parameter_values                   = self._get_param_vec_initial()
 
         self.index_normparam_map                        = self._make_map_index_norm()
+        self.index_langrangeparam_map                   = self._make_map_index_lag()
 
         self.yield_array_dict, _                        = self._get_nominal_expected_arrays( type_of_fit = "binned" )
         self.unbinned_total_dict, \
@@ -175,6 +176,16 @@ class sbi_parametric_model:
             dict_index_normfactor[normfactor] = index
         return dict_index_normfactor
 
+    def _make_map_index_lag(self):
+        """
+        Maps the index of parameter in the parameter vector to norm factor
+        """
+        dict_index_langrangefactor = {}
+        for langrangefactor in self.list_langrange_coeffs:
+            index = self.list_parameters.index( langrangefactor )
+            dict_index_langrangefactor[langrangefactor] = index
+        return dict_index_langrangefactor
+
     def _get_param_vec_initial(self):
         initial_values_vec                     = np.ones((len(self.list_parameters),)) 
         for count, parameter in enumerate(self.list_parameters):
@@ -221,14 +232,13 @@ class sbi_parametric_model:
                         if modifier_name not in list_all_lagrange_factors               : list_all_lagrange_factors.append(modifier_name)
                         if modifier_name not in dict_sample_lagrange_factors[sample]     : dict_sample_lagrange_factors[sample][modifier_name] = modiefier_coeffs
 
-        list_all_lagrange_factors = [p for p in list_all_norm_factors if p in self.param_names]
-        dict_sample_lagrange_factors = {key: val for key, val in dict_sample_normfactors.items()
+        list_all_lagrange_factors = [p for p in list_all_lagrange_factors if p in self.param_names]
+        dict_sample_lagrange_factors = {key: val for key, val in dict_sample_lagrange_factors.items()
                                     if any(p in self.param_names for p in val)
                                 }
         
         return list_all_lagrange_factors, dict_sample_lagrange_factors
 
-        return list_all_norm_factors, dict_sample_normfactors    
     def _get_parameters_to_fit(self) -> tuple[list[str], dict[str, float]]:
         """
         Outputs a list of parameters specified by the user for fitting in the workspace
@@ -474,8 +484,7 @@ class sbi_parametric_model:
         for sample , params_sample in self.langrange_coeffs_map.items():
             # params_sample is a dict of param name to list of lagrange coefficients
             for param, coeffs in params_sample.items():
-                index_param             = self.index_normparam_map[param]
-                coeffs                  = self.langrange_coeffs.get(param, [1,0])
+                index_param             = self.index_langrangeparam_map[param]
                 norm_var[sample]       *= np.polyval(coeffs, param_vec[index_param])
                 
         return norm_var
@@ -603,6 +612,10 @@ class sbi_parametric_model:
             if sample in self.norm_sample_map:
                 for nf_name in self.norm_sample_map[sample]:
                     j = self.index_normparam_map[nf_name]
+                    norm_matrix[i, j] = True
+            if sample in self.langrange_coeffs_map:
+                for lf_name in self.langrange_coeffs_map[sample]:
+                    j = self.index_langrangeparam_map[lf_name]
                     norm_matrix[i, j] = True
 
         # Bundle everything into a dict pytree passed as a *dynamic* argument

src/nsbi_common_utils/workspace_builder.py
@@ -58,7 +58,7 @@ class WorkspaceBuilder:
                              sample_name: str) -> list[dict[str, Any]]:
         """Return normfactor modifiers that affect a given sample in a region.
 
-        Iterates over all ``NormFactors`` in the configuration and keeps only those whose ``Region`` and ``Samples`` lists include the requested region/sample (or are unset, meaning they apply everywhere).
+        Iterates over all ``LagrangeFactors`` in the configuration and keeps only those whose ``Region`` and ``Samples`` lists include the requested region/sample (or are unset, meaning they apply everywhere).
 
         Parameters
         ----------
@@ -391,7 +391,7 @@ class WorkspaceBuilder:
     def measurements(self) -> List[Dict[str, Any]]:
         """Build the ``"measurements"`` list for the workspace.
 
-        Extracts parameter initial values and bounds from the ``NormFactors`` and ``Systematics`` sections of the config, filters to the ``ParametersToFit`` subset (if specified), and records the parameter of interest (POI).
+        Extracts parameter initial values and bounds from the ``NormFactors`` ,``LagrangeFactors`` and ``Systematics`` sections of the config, filters to the ``ParametersToFit`` subset (if specified), and records the parameter of interest (POI).
 
         Returns
         -------
@@ -405,6 +405,19 @@ class WorkspaceBuilder:
 
         # get the norm factor initial values / bounds / constant setting
         parameters_list = []
+        for lf in self.config_dict.get("LagrangeFactors", []):
+            lf_name = lf["Name"]  # every LagrangeFactors has a name
+            init = lf.get("Nominal", None)
+            bounds = lf.get("Bounds", None)
+
+            parameter = {"name": lf_name}
+            if init is not None:
+                parameter.update({"inits": [init]})
+            if bounds is not None:
+                parameter.update({"bounds": [bounds]})
+
+            parameters_list.append(parameter)
+
         for nf in self.config_dict.get("NormFactors", []):
             nf_name = nf["Name"]  # every NormFactor has a name
             init = nf.get("Nominal", None)
@@ -515,13 +528,13 @@ class WorkspaceBuilder:
             return json.load(f)
 
 
-def define_stat_function(kls):
+def define_stat_function(kls_dict):
     stat_coeff = {}
+    
+    kls = list(kls_dict.keys())
 
     A = np.array([[k**2, k, 1] for k in kls])
     A_inv = np.linalg.inv(A)
-
-    kls_dict = get_basis_tags(kls)
 
     for i, kl in enumerate(kls):
         key = kls_dict[kl]